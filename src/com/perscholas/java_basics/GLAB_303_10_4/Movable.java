15228146db3a6d3a00ba9986974a454629cb00c9
Esther Yoo <liletchie@gmail.com>
2024-04-29 15:16:46 -0400

    GLAB 303.10.4

---
+++ src/com/perscholas/java_basics/GLAB_303_10_4/Movable.java
@@ -0,0 +1,9 @@
+package com.perscholas.java_basics.GLAB_303_10_4;
+
+public interface Movable {
+    void moveUp();    // "public" and "abstract" by default
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+    String getCoordinate();
+}
