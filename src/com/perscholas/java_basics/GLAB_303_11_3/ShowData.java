be2020ec0edd4a0e24ffc2cca608526abfc49eb2
Esther Yoo <liletchie@gmail.com>
2024-05-02 15:24:03 -0400

    GLAB 3030.11.3- ArrayList of User-Defined Objects

---
+++ src/com/perscholas/java_basics/GLAB_303_11_3/ShowData.java
@@ -0,0 +1,17 @@
+package com.perscholas.java_basics.GLAB_303_11_3;
+
+import java.util.ArrayList;
+
+public class ShowData {
+    public static void main(String[] args) {
+        // instantioation to class AddDataToArrayList
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> mybooklist = b.bookdetails();
+        for(Book showValue: mybooklist)
+        {
+            // ---- invoking getter method for geting Data---------
+            System.out.println(showValue.getNumber() + " " + showValue.getName() +" "+ showValue.getCategory() +" "+ showValue.getAuthor());
+        }
+    }
+
+}
